3b4aff637d7437da4054e16d03bf7c87963b9143
motiman972 <73071524+motiman972@users.noreply.github.com>
2020-12-22 14:03:41 +0200

    Add files via upload

---
+++ panel.php
@@ -0,0 +1,45 @@
+﻿  <div class="panel12">
+    <div class="text12">שווה לבקר<br>‍</div>
+    <a href="https://tchumim.com/" target="_blank" ><img src="https://mitmachim.top/assets/uploads/files/1602702078674-tchumim.png" alt="" class="img12"></a>
+    <a href="https://www.prog.co.il/" target="_blank" ><img src="https://mitmachim.top/assets/uploads/files/1602702061746-prog.png" alt="" class="img12"></a>
+    <a href="https://www.tora-forum.co.il/" target="_blank" ><img src="https://www.tora-forum.co.il/ext/tatiana5/favicon/favicon.ico" alt="" class="img12"></a>
+    <a href="https://mitmachim.top/" target="_blank" ><img src="https://mitmachim.top/assets/uploads/files/1602745229075-mtm.png" alt="" class="img12"></a>
+    <a href="https://forum-office.ml/" target="_blank" ><img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Microsoft_Office_logo_%282019%E2%80%93present%29.svg" alt="" class="img12"></a>
+    <a href="https://xn--9dbazrsfz.com/"target="_blank" ><img src="https://mitmachim.top/assets/uploads/files/1602701994040-mk.png" alt="" class="img12"></a>
+    <a href="https://f2.freeivr.co.il/"target="_blank" ><img src="https://mitmachim.top/assets/uploads/files/1602704764319-%D7%99%D7%9E%D7%95%D7%AA.png" alt="" class="img12"></a></div>
+ 
+<style> 
+.panel12 {
+  position: fixed;
+  display: flex;
+  width: 60px;
+  height: auto;
+  left: 0px;
+  bottom: 80px;
+  padding-top: 12px;
+  padding-bottom: 8px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-top-right-radius:20px;
+  border-bottom-right-radius: 20px;
+  background-color: #e4e8ff;
+}
+ 
+.text12 {
+  display: block;
+  margin-right: auto;
+  margin-bottom: 6px;
+  margin-left: auto;
+  @import url('<link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Varela+Round&display=swap" rel="stylesheet">');
+  font-family: 'Varela Round', sans-serif;
+  line-height: 18px;
+  font-weight: 700;
+  text-align: center;
+}
+ 
+.img12 {
+  width: 37px;
+  margin-bottom: 15px;
+}
+</style>
